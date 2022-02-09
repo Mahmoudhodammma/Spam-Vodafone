550e97aa4f5b89af34e3f95df52a0166f9d92692
Mahmoudhodammma <91755885+Mahmoudhodammma@users.noreply.github.com>
2022-02-09 19:57:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,26 @@
+import flask
 import requests
 from flask import *
 app = Flask(__name__)
-@app.route("/spam/vodafone/")
-def f():
-	nu = request.args.get("Number")
-	url =  'https://arabia.starzplay.com/api/esb/userAccount/MSISDN/verify'
-	headers = {
-    "Host": "arabia.starzplay.com",
-    "content-length": "86",
-    "sec-ch-ua": "\" Not A;Brand\";v\u003d\"99\", \"Chromium\";v\u003d\"90\", \"Google Chrome\";v\u003d\"90\"",
-    "accept": "application/json, text/javascript, */*; q\u003d0.01",
-    "x-requested-with": "XMLHttpRequest",
-    "sec-ch-ua-mobile": "?1",
-    "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36",
-    "content-type": "application/x-www-form-urlencoded; charset\u003dUTF-8",
-    "origin": "https://arabia.starzplay.com",
-    "sec-fetch-site": "same-origin",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://arabia.starzplay.com/ar/partners/vodafone-egypt",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q\u003d0.9,ar-EG;q\u003d0.8,ar;q\u003d0.7",
-    "cookie": "_gat_UA-52364929-4\u003d1"
+@app.route("/id/instagram/")
+def fj():
+	User = request.args.get("User")
+	head = {
+'HOST': "www.instagram.com",
+'KeepAlive' : 'True',
+'user-agent' : "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36",
+'Cookie': 'cookie',
+'Accept' : "*/*",
+'ContentType' : "application/x-www-form-urlencoded",
+"X-Requested-With" : "XMLHttpRequest",
+"X-IG-App-ID": "936619743392459",
+"X-Instagram-AJAX" : "missing",
+"X-CSRFToken" : "missing",
+"Accept-Language" : "en-US,en;q=0.9"
 }
-	data = {
-    'mobilePrefix' : '20' ,
-    'mobileNumber':nu,
-    'operator' : 'vodafoneegypt' 
-}
-	response = requests.post(url,headers=headers,data=data).text
-	if ('smsTransactionId') in response:
-		return jsonify(Spam=True,Telegram="@uufffuu")
-	else:
-		return jsonify(Spam=False,Telegram="@uufffuu")
-if __name__ == "__name__":
+	url_id = f'https://www.instagram.com/{User}/?__a=1'
+	req_id = requests.get(url_id,headers=head).json()
+	idd  = str(req_id['graphql']['user']['id'])
+	return jsonify(Telegram="@uufffuu",ID=idd)
+if __name__=="__main__":
 	app.run()
